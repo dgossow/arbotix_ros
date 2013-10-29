c6bef8c4b2aaec74f3a27052f3a3eecd336de786
David Gossow <dgossow@willowgarage.com>
2013-10-28 21:47:49 -0700

    added camera trigger on user address 100

--- arbotix_firmware/src/user_hooks.h
+++ arbotix_firmware/src/user_hooks.h
@@ -3,16 +3,26 @@
  * Just use addresses 100 and above
  */
 
+int trigger = 16;
+
 void userSetup()
 {
   // do any setup here
+  pinMode(trigger, OUTPUT);  // trigger pin
 }
 
 unsigned char userWrite(int addr, unsigned char param)
 {
   // use the register value
+  if ( addr == 100 )
+  {
+    digitalWrite(trigger,LOW);
+    delay(1+param);
+    digitalWrite(trigger,HIGH);
+  }
   return ERR_INSTRUCTION;
 }
+
 
 /* Read one byte from register located at addr */
 int userRead(int addr)
